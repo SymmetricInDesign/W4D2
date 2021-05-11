2c00acb12eb25c789f4c974182669115acaebd02
taihuynh812 <taihuynh@revlisoft.com>
2021-05-11 13:58:17 -0400

    add knight and king movements

--- chess/pieces/knight.rb
+++ chess/pieces/knight.rb
@@ -2,12 +2,21 @@ require_relative "../piece"
 require "./steppable"
 
 class Knight
-    
+    ROW = [1, 1,-1,-1, 2, 2,-2,-2]
+    COL = [2,-2, 2,-2, 1,-1, 1,-1]
+
+    def initialize(color, board, pos)
+        super
+    end
 
     def valid_moves
         arr = []
 
         arr << move if super(move)
-
     end
+
+    def symbol
+        :K
+    end
+    
 end
