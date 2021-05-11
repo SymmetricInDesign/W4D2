89f4e04d34933e574a8fc9c8f7dde11ace741215
Donnie Wombough <donniewomboughjunior@yahoo.com>
2021-05-11 13:31:57 -0400

    add class skeletons

--- chess/board.rb
+++ chess/board.rb
@@ -1,3 +1,26 @@
 class Board
-    
+
+    def initialize()
+        @grid = Array.new(8){Array.new(8)}
+
+    end
+
+    def add_piece(piece, pos)
+        self[pos] = piece
+    end
+
+    def move_piece(start_pos, end_pos)
+
+    end
+
+    def [](pos)
+        x,y=pos
+        @grid[x][y]
+    end
+
+    def []=(pos, value)
+        x,y=pos
+        @grid[x][y] = value
+    end
+
 end
